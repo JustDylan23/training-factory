fad8eaab0d9a30d6f6d831f29365479d9e9595ba
JustDylan23 <dylanvdhout@gmail.com>
2020-01-07 15:25:19 +0100

    small update

--- public/js/delete_entry.js
+++ public/js/delete_entry.js
@@ -1,17 +1,15 @@
 $(document).ready(function (e) {
     $('.button-remove-entry').click(function (e) {
-        $('#delete-confirm-button').attr('href', $(e.currentTarget).attr('data-href'));
+        $('#delete-confirm-button')
+            .attr('href', $(e.currentTarget).attr('data-href'))
     });
-    $('#delete-confirm-button').click(function (e) {
+    $('#delete-confirm-button').click(async function (e) {
         e.preventDefault();
         let link = $(e.currentTarget).attr('href');
-        $.ajax({
-            method: 'POST',
-            url: link
-        }).done((data) => {
-            if (data.success === true) {
-                $(`[data-link='${link}']`).closest('.entry').remove();
-            }
-        });
+        let response = await fetch(link, {method: 'POST'});
+        let data = await response.json();
+        if (data.success === true) {
+            location.reload();
+        }
     });
 });
